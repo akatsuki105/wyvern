f426afa8a18e32d69ee3cf5b888d55b2a01db88f
pokemium <bluejapan73+dev@gmail.com>
2021-01-12 10:19:54 +0900

    feat: Implement decompress in RGBASM format

--- asm/decompress.asm
+++ asm/decompress.asm
@@ -1,21 +1,25 @@
 ; GB-Compress decompressor in RGBASM format.
-; NOT Compatible with GBTD
+; Compatible with GBTD
 
 _gb_decompress::
-    lda     hl, 5(SP)
-    ld      a, (hl-)
+    ld      hl, sp+5
+    ; de = [sp+5] << 8 | [sp+4] = dest
+    ld      a, [hld]
     ld      d, a
-    ld      a, (hl-)
+    ld      a, [hld]
     ld      e, a
-    ld      a, (hl-)
-    ld      l, (hl)
+    ; hl = [sp+3] | [sp+2]
+    ld      a, [hld]
+    ld      l, [hl]
     ld      h, a
+    ; fallthrough
 
 ; hl = source; de = dest
+; bc is not changed
 gb_decompress::
     push    bc
 .loop
-    ld      a, (hl+)     ; load command
+    ld      a, [hli]     ; load command
     or      a
     jr      z, .exit    ; exit, if last byte
     bit     7, a
@@ -26,9 +30,9 @@ gb_decompress::
     and     %00111111         ; calc counter
     inc     a
     ld      b, a
-    ld      a, (hl+)
+    ld      a, [hli]
 .byteLoop
-    ld      (de), a
+    ld      [de], a
     inc     de
     dec     b
     jr      nz, .byteLoop
@@ -36,17 +40,17 @@ gb_decompress::
 .word                   ; RLE word
     and     %00111111
     inc     a
-    ld      b, (hl)  ; load word into bc
+    ld      b, [hl]  ; load word into bc
     inc     hl
-    ld      c, (hl)
+    ld      c, [hl]
     inc     hl
 .wordLoop
     push    af
     ld      a, b     ; store word
-    ld      (de), a
+    ld      [de], a
     inc     de
     ld      a, c
-    ld      (de), a
+    ld      [de], a
     inc     de
     pop     af
     dec     a
@@ -59,16 +63,16 @@ gb_decompress::
     and     a, %00111111
     inc     a
     push    hl
-    ld      c, (hl)  ; bc = offset
+    ld      c, [hl]  ; bc = offset
     inc     hl
-    ld      b, (hl)
+    ld      b, [hl]
     ld      h, d     ; hl = de(dest)
     ld      l, e
     add     hl, bc   ; hl(from) = hl(dest) + bc(offset)
     ld      b, a
 .stringLoop
-    ld      a, (hl+)
-    ld      (de), a
+    ld      a, [hli]
+    ld      [de], a
     inc     de
     dec     b
     jr      nz, .stringLoop
@@ -81,8 +85,8 @@ gb_decompress::
     inc     a
     ld      b, a
 .trashLoop                    
-    ld      a, (hl+)
-    ld      (de), a
+    ld      a, [hli]
+    ld      [de], a
     inc     de
     dec     b
     jr      nz, .trashLoop
